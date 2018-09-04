0768a709b4d6610441def89f0f3a46b8f48ac339
root <root@servermcserverson.lan>
2018-09-04 20:57:55 +0000

    started working on login page and implementing bootstrap

--- __init__.py
+++ __init__.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
 app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "Hello, I love Digital Ocean!"
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return "Logged in"
+ 
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('Wrong password')
+    return home()
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
